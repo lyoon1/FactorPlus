8f773894733a87191ae82ad2d298ce1c2418d3ad
Leo Yoon <LionKing@leos-macbook-air.local>
2015-10-16 09:14:23 -0400

    Test

--- Factor+/DataViewController.swift
+++ Factor+/DataViewController.swift
@@ -16,6 +16,7 @@ class DataViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("test")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
