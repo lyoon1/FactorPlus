abac3ece7a9816b2f142ef185c6a6c93a33cd487
Leo Yoon <LionKing@leos-macbook-air.local>
2015-10-19 10:29:14 -0400

    Removed Test prints

--- Factor+/DataViewController.swift
+++ Factor+/DataViewController.swift
@@ -16,10 +16,6 @@ class DataViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("test")
-        print("john was here")
-        print("Leo Was here")
-        print("Am I here?")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
